ddebc8085b8116a0f0b6e239894927a7223a566e
Albert <jiuzheyanglianxiwo@gmail.com>
2025-10-18 21:26:33 +0800

    Update PinyinCustomMetadataProvider.cs

--- Providers/PinyinCustomMetadataProvider.cs
+++ Providers/PinyinCustomMetadataProvider.cs
@@ -18,14 +18,14 @@ namespace EmbyPinyinPlugin.Providers
     /// 为电影元数据提供拼音首字母排序功能。
     /// 实现 ICustomMetadataProvider 和 IHasOrder 以在元数据合并后期进行干预，并确保优先级。
     /// </summary>
-    public class PinyinCustomMetadataProvider : ICustomMetadataProvider<Movie>, IHasOrder
+    public class PinyinCustomMetadataProviderMovie : ICustomMetadataProvider<Movie>, IHasOrder
     {
         // 无参数构造函数 (如果需要依赖注入，请添加相应参数)
-        public PinyinCustomMetadataProvider()
+        public PinyinCustomMetadataProviderMovie()
         {
         }
 
-        public string Name => "Pinyin Custom Sorter";
+        public string Name => "Pinyin Custom Sorter Movie";
 
         // 实现 IHasOrder 接口，返回较低的数字以获得较高优先级
         public int Order => 0; // 在同类提供者中拥有最高优先级
@@ -60,11 +60,13 @@ namespace EmbyPinyinPlugin.Providers
             item.SetSortNameDirect(pinyinInitials.ToUpper()); // 关键：设置拼音首字母
 
             // 5. 返回更新类型，告知 Emby 我们更新了元数据
-            return ItemUpdateType.MetadataEdit; // 或者 ItemUpdateType.MetadataImport, 根据具体意图
+            return ItemUpdateType.MetadataEdit;
         }
     }
 
-    // 你可以为其他类型创建类似的提供者
+    /// <summary>
+    /// 为电视剧元数据提供拼音首字母排序功能。
+    /// </summary>
     public class PinyinCustomMetadataProviderSeries : ICustomMetadataProvider<Series>, IHasOrder
     {
         public PinyinCustomMetadataProviderSeries()
@@ -99,5 +101,216 @@ namespace EmbyPinyinPlugin.Providers
         }
     }
 
-    // ... 为其他类型 (Episode, MusicAlbum, MusicArtist, Video, Photo, BoxSet) 创建提供者 ...
+    /// <summary>
+    /// 为剧集元数据提供拼音首字母排序功能。
+    /// </summary>
+    public class PinyinCustomMetadataProviderEpisode : ICustomMetadataProvider<Episode>, IHasOrder
+    {
+        public PinyinCustomMetadataProviderEpisode()
+        {
+        }
+
+        public string Name => "Pinyin Custom Sorter Episode";
+
+        public int Order => 0;
+
+        public async Task<ItemUpdateType> FetchAsync(
+            MetadataResult<Episode> itemResult,
+            MetadataRefreshOptions options,
+            LibraryOptions libraryOptions,
+            CancellationToken cancellationToken)
+        {
+            var item = itemResult.Item;
+            var nameToProcess = item.Name; // 或 item.OriginalTitle
+            if (string.IsNullOrEmpty(nameToProcess))
+            {
+                return ItemUpdateType.None;
+            }
+
+            string pinyinInitials = PinyinHelper.GetPinyinInitials(nameToProcess);
+            if (string.IsNullOrEmpty(pinyinInitials))
+            {
+                 return ItemUpdateType.None;
+            }
+
+            item.SetSortNameDirect(pinyinInitials.ToUpper());
+            return ItemUpdateType.MetadataEdit;
+        }
+    }
+
+    // ... 为其他类型 (MusicAlbum, MusicArtist, Video, Photo, BoxSet) 创建提供者 ...
+    // 例如 MusicAlbum
+    public class PinyinCustomMetadataProviderMusicAlbum : ICustomMetadataProvider<MusicAlbum>, IHasOrder
+    {
+        public PinyinCustomMetadataProviderMusicAlbum()
+        {
+        }
+
+        public string Name => "Pinyin Custom Sorter MusicAlbum";
+
+        public int Order => 0;
+
+        public async Task<ItemUpdateType> FetchAsync(
+            MetadataResult<MusicAlbum> itemResult,
+            MetadataRefreshOptions options,
+            LibraryOptions libraryOptions,
+            CancellationToken cancellationToken)
+        {
+            var item = itemResult.Item;
+            var nameToProcess = item.Name; // 或 item.OriginalTitle
+            if (string.IsNullOrEmpty(nameToProcess))
+            {
+                return ItemUpdateType.None;
+            }
+
+            string pinyinInitials = PinyinHelper.GetPinyinInitials(nameToProcess);
+            if (string.IsNullOrEmpty(pinyinInitials))
+            {
+                 return ItemUpdateType.None;
+            }
+
+            item.SetSortNameDirect(pinyinInitials.ToUpper());
+            return ItemUpdateType.MetadataEdit;
+        }
+    }
+
+    // 例如 MusicArtist
+    public class PinyinCustomMetadataProviderMusicArtist : ICustomMetadataProvider<MusicArtist>, IHasOrder
+    {
+        public PinyinCustomMetadataProviderMusicArtist()
+        {
+        }
+
+        public string Name => "Pinyin Custom Sorter MusicArtist";
+
+        public int Order => 0;
+
+        public async Task<ItemUpdateType> FetchAsync(
+            MetadataResult<MusicArtist> itemResult,
+            MetadataRefreshOptions options,
+            LibraryOptions libraryOptions,
+            CancellationToken cancellationToken)
+        {
+            var item = itemResult.Item;
+            var nameToProcess = item.Name; // 或 item.OriginalTitle
+            if (string.IsNullOrEmpty(nameToProcess))
+            {
+                return ItemUpdateType.None;
+            }
+
+            string pinyinInitials = PinyinHelper.GetPinyinInitials(nameToProcess);
+            if (string.IsNullOrEmpty(pinyinInitials))
+            {
+                 return ItemUpdateType.None;
+            }
+
+            item.SetSortNameDirect(pinyinInitials.ToUpper());
+            return ItemUpdateType.MetadataEdit;
+        }
+    }
+
+    // 例如 Video (独立视频文件)
+    public class PinyinCustomMetadataProviderVideo : ICustomMetadataProvider<Video>, IHasOrder
+    {
+        public PinyinCustomMetadataProviderVideo()
+        {
+        }
+
+        public string Name => "Pinyin Custom Sorter Video";
+
+        public int Order => 0;
+
+        public async Task<ItemUpdateType> FetchAsync(
+            MetadataResult<Video> itemResult,
+            MetadataRefreshOptions options,
+            LibraryOptions libraryOptions,
+            CancellationToken cancellationToken)
+        {
+            var item = itemResult.Item;
+            var nameToProcess = item.Name; // 或 item.OriginalTitle
+            if (string.IsNullOrEmpty(nameToProcess))
+            {
+                return ItemUpdateType.None;
+            }
+
+            string pinyinInitials = PinyinHelper.GetPinyinInitials(nameToProcess);
+            if (string.IsNullOrEmpty(pinyinInitials))
+            {
+                 return ItemUpdateType.None;
+            }
+
+            item.SetSortNameDirect(pinyinInitials.ToUpper());
+            return ItemUpdateType.MetadataEdit;
+        }
+    }
+
+    // 例如 Photo (独立图片文件)
+    public class PinyinCustomMetadataProviderPhoto : ICustomMetadataProvider<Photo>, IHasOrder
+    {
+        public PinyinCustomMetadataProviderPhoto()
+        {
+        }
+
+        public string Name => "Pinyin Custom Sorter Photo";
+
+        public int Order => 0;
+
+        public async Task<ItemUpdateType> FetchAsync(
+            MetadataResult<Photo> itemResult,
+            MetadataRefreshOptions options,
+            LibraryOptions libraryOptions,
+            CancellationToken cancellationToken)
+        {
+            var item = itemResult.Item;
+            var nameToProcess = item.Name; // 或 item.OriginalTitle
+            if (string.IsNullOrEmpty(nameToProcess))
+            {
+                return ItemUpdateType.None;
+            }
+
+            string pinyinInitials = PinyinHelper.GetPinyinInitials(nameToProcess);
+            if (string.IsNullOrEmpty(pinyinInitials))
+            {
+                 return ItemUpdateType.None;
+            }
+
+            item.SetSortNameDirect(pinyinInitials.ToUpper());
+            return ItemUpdateType.MetadataEdit;
+        }
+    }
+
+    // 例如 BoxSet (合集)
+    public class PinyinCustomMetadataProviderBoxSet : ICustomMetadataProvider<BoxSet>, IHasOrder
+    {
+        public PinyinCustomMetadataProviderBoxSet()
+        {
+        }
+
+        public string Name => "Pinyin Custom Sorter BoxSet";
+
+        public int Order => 0;
+
+        public async Task<ItemUpdateType> FetchAsync(
+            MetadataResult<BoxSet> itemResult,
+            MetadataRefreshOptions options,
+            LibraryOptions libraryOptions,
+            CancellationToken cancellationToken)
+        {
+            var item = itemResult.Item;
+            var nameToProcess = item.Name; // 或 item.OriginalTitle
+            if (string.IsNullOrEmpty(nameToProcess))
+            {
+                return ItemUpdateType.None;
+            }
+
+            string pinyinInitials = PinyinHelper.GetPinyinInitials(nameToProcess);
+            if (string.IsNullOrEmpty(pinyinInitials))
+            {
+                 return ItemUpdateType.None;
+            }
+
+            item.SetSortNameDirect(pinyinInitials.ToUpper());
+            return ItemUpdateType.MetadataEdit;
+        }
+    }
 }
